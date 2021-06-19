88d94498e5c296a23c656e03e8a8cef4dc9f48b0
rebecca-agyei <rebecca.agyei@amalitech.org>
2021-06-19 22:12:51 +0000

    Commit of my Leap Year Calculator

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let check = document.getElementById("check")
+let output = document.getElementById("output")
+
+const leapYear = () => {
+    let yearInput = document.getElementById("year").value
+
+    if (yearInput % 4 === 0) {
+        if (yearInput % 100 === 0) {
+          if (yearInput % 400 === 0) {
+            document.getElementById("output").innerHTML = "The year " + yearInput + " is a leap year."
+          } else {
+            document.getElementById("output").innerHTML = "The year " + yearInput + " is not a leap year."
+          }
+        } else {
+            document.getElementById("output").innerHTML = "The year " + yearInput + " is a leap year."
+        }
+      } else {
+        document.getElementById("output").innerHTML = "The year " + yearInput + " is not a leap year."
+      }
+    }
+
+
+    check.addEventListener('click',()=>{leapYear()})
